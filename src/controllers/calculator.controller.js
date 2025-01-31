28a5761e5165d064160cc34821e6952cb3a8774a
ariqhikari <ahidayatbia@gmail.com>
2025-01-31 10:56:57 +0700

    fix: history calculations

--- src/controllers/calculator.controller.js
+++ src/controllers/calculator.controller.js
@@ -115,7 +115,7 @@ const calculateCalories = async (req, res) => {
 
 const getHistoryCalculations = async (req, res) => {
   try {
-    const result = await history_calculation_model.find({
+    const result = await history_calculation_model.findAll({
       where: { userId: jwt.decode(req.headers.token).id },
     });
 
